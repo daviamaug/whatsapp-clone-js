79d87b9d1587f0547dab6fff034ff4d6e38d8d38
Davi A. Augusto <daviamaug@gmail.com>
2020-06-24 19:47:14 -0300

    first commit

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,80 @@
+class WhatsAppController {
+
+  constructor() {
+
+    this.elementsPrototype()
+    this.loadElements()
+
+  }
+
+  loadElements() {
+
+    this.el = {}
+
+    document.querySelectorAll('[id]').forEach(element => {
+
+      this.el[Format.getCamelCase(element.id)] = element
+
+    })
+
+  }
+  elementsPrototype() {
+
+    Element.prototype.hide = function () {
+
+      this.style.display = 'none'
+      return this
+    }
+    Element.prototype.show = function () {
+
+      this.style.display = 'block'
+      return this
+
+    }
+    Element.prototype.toggle = function () {
+
+      this.style.display = (this.style.display === 'none') ? 'block' : 'none'
+      return this
+    }
+    Element.prototype.on = function (events, fn) {
+
+      events.split(' ').forEach(e => {
+
+        this.addEventListener(e, fn)
+
+      })
+      return this
+    }
+    Element.prototype.css = function (styles) {
+
+      for (let name in styles) {
+
+        this.style[name] = styles[name]
+
+      }
+      return this
+    }
+    Element.prototype.addClass = function (name) {
+
+      this.classList.add(name)
+      return this
+    }
+    Element.prototype.delClass = function (name) {
+
+      this.classList.remove(name)
+      return this
+    }
+    Element.prototype.toggleClass = function (name) {
+
+      this.classList.toggle(name)
+      return this
+    }
+    Element.prototype.hasClass = function (name) {
+
+      return this.classList.contains(name)
+
+    }
+
+  }
+
+}
