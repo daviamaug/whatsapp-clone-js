516519785caf6f0824d7da025efcf95b8466feb8
Davi A. Augusto <daviamaug@gmail.com>
2020-06-25 19:49:41 -0300

    add method to drawer

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
     this.elementsPrototype()
     this.loadElements()
+    this.initEvents()
 
   }
 
@@ -59,7 +60,7 @@ class WhatsAppController {
       this.classList.add(name)
       return this
     }
-    Element.prototype.delClass = function (name) {
+    Element.prototype.removeClass = function (name) {
 
       this.classList.remove(name)
       return this
@@ -74,6 +75,130 @@ class WhatsAppController {
       return this.classList.contains(name)
 
     }
+    HTMLFormElement.prototype.getForm = function () {
+
+      return new FormData(this)
+
+    }
+    HTMLFormElement.prototype.toJSON = function () {
+
+      let json = {}
+
+      this.getForm().forEach((value, key) => {
+
+        json[key] = value
+
+      })
+
+      return json
+
+    }
+
+  }
+  initEvents() {
+
+    this.el.myPhoto.on('click', e => {
+
+      this.closeAllLeftPanel()
+      this.el.panelEditProfile.show()
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass('open')
+      }, 100)
+
+    })
+    this.el.btnNewContact.on('click', e => {
+
+      this.closeAllLeftPanel()
+      this.el.panelAddContact.show()
+      setTimeout(() => {
+        this.el.panelAddContact.addClass('open')
+      }, 100)
+
+    })
+    this.el.btnClosePanelEditProfile.on('click', e => {
+
+      this.el.panelEditProfile.removeClass('open')
+
+    })
+    this.el.btnClosePanelAddContact.on('click', e => {
+
+      this.el.panelAddContact.removeClass('open')
+
+    })
+    this.el.photoContainerEditProfile.on('click', e => {
+
+      this.el.inputProfilePhoto.click()
+
+    })
+    this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+      if (e.key === 'Enter') {
+
+        e.preventDefault()
+        this.el.btnSavePanelEditProfile.click()
+
+      }
+
+    })
+    this.el.btnSavePanelEditProfile.on('click', e => {
+
+      console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+
+    })
+    this.el.formPanelAddContact.on('submit', e => {
+
+      e.preventDefault()
+      let formData = new FormData(this.el.formPanelAddContact)
+
+    })
+    this.el.contactsMessagesList.querySelectorAll(".contact-item").forEach(item => {
+
+      item.on('click', e => {
+
+        this.el.home.hide()
+        this.el.main.css({
+          display: 'flex'
+        })
+      })
+
+    })
+    this.el.btnAttach.on('click', e => {
+
+      e.stopPropagation()
+      this.el.menuAttach.addClass('open')
+      document.addEventListener('click', this.closeMenuAttach.bind(this))
+    })
+    this.el.btnAttachPhoto.on('click', e => {
+
+
+
+    })
+    this.el.btnAttachCamera.on('click', e => {
+
+
+
+    })
+    this.el.btnAttachDocument.on('click', e => {
+
+
+
+    })
+    this.el.btnAttachContact.on('click', e => {
+
+
+
+    })
+
+  }
+  closeMenuAttach(e) {
+    document.removeEventListener('click', this.closeMenuAttach)
+    this.el.menuAttach.removeClass('open')
+  }
+  closeAllLeftPanel() {
+
+    this.el.panelEditProfile.hide()
+    this.el.panelAddContact.hide()
 
   }
 
